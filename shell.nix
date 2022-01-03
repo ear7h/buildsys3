f7fb7614e8d2ba48ca2b9572df599397c786909c
julio <julio.grillo98@gmail.com>
2022-01-03 18:13:25 +0000

    use cabal2nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,33 @@
-with import <nixpkgs> {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
-stdenv.mkDerivation {
-  name = "buildsys3";
+let
 
-  buildInputs = [ zlib.dev ];
+  inherit (nixpkgs) pkgs;
 
-  # set the LD_LIBRARY_PATH so that the program can find wayland gui libs
-  shellHook = ''
-    exec zsh
-  '';
-}
+  f = { mkDerivation, base, dhall, filepath, lib, mtl
+      , optparse-applicative, process, text, unix
+      }:
+      mkDerivation {
+        pname = "buildsys3";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base dhall filepath mtl optparse-applicative process text unix
+        ];
+        license = "unknown";
+        hydraPlatforms = lib.platforms.none;
+      };
 
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
